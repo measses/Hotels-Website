b030c5bc04f61f923f3ab8cf5b03d80d0158961c
Mert Araz <mertoreyiz1907@gmail.com>
2023-08-02 14:25:12 +0300

    landing-page finished

---
+++ js/index.js
@@ -0,0 +1,43 @@
+ScrollReveal().reveal('.gallery-item', {
+    distance: '50px',
+    origin: 'left',
+    interval: 250,
+  });
+
+  ScrollReveal().reveal('.contact-form', {
+    distance: '100px',
+    origin: 'right',
+    duration: 1000,
+  });
+  
+  ScrollReveal().reveal('.about', {
+    distance: '100px',
+    origin: 'right',
+    duration: 1500,
+  });
+
+  ScrollReveal().reveal('.services', {
+    distance: '100px',
+    origin: 'bottom',
+    duration: 1000,
+  });
+
+  ScrollReveal().reveal('.footer-contact', {
+    distance: '100px',
+    origin: 'right',
+    duration: 1100,
+  });
+
+  
+  ScrollReveal().reveal('.footer-links', {
+    distance: '100px',
+    origin: 'left',
+    duration: 1200,
+  });
+
+  
+  ScrollReveal().reveal('.footer-social', {
+    distance: '100px',
+    origin: 'left',
+    duration: 1300,
+  });
